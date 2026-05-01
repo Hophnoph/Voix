449d04c850b2ee8fc6230fb8c885eaf110bb7bdb
hophnoph <collectablles@protonmail.com>
2026-05-01 22:31:06 +0100

    example lualib config for voix

--- assets/lualib/voix.lua
+++ assets/lualib/voix.lua
@@ -1,1 +1,44 @@
 -- voix lua config library
+
+-- local services = require("assets.lualib.services")
+local voix = {}
+
+voix.xbps = {
+	pkgs = {
+		--"go", "git", "neovim"
+		-- each of the packages in here will be tracked in a root owned file
+	},
+}
+
+voix.system = {
+	hostname = "",
+	keyboard = {
+		tty_keyboard = "",
+		x_keyboard = "",
+	},
+	glibc_locales = "",
+	timezone = "",
+	users = {
+		--example_user = {
+		--groups = { "wheel", "example_users", "users" },
+		--},
+	},
+}
+
+--services:load() to load services
+voix.runit = {
+	enabled_services = {
+		--services.sv.elogind
+		--services.sv.lightdm
+		--services.sv.etc services
+	},
+}
+
+voix.voix_install = {
+	--[[
+  the table where voix-install command will get install only options
+  so when runing voix-install it looks for it, and installs system
+]]
+}
+
+return voix
