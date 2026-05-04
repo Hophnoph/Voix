9fa8a98b9d97c993d8fd34ca18f47e45f9334f65
hophnoph <collectablles@protonmail.com>
2026-05-04 18:56:25 +0100

    fixed some bugs and made a proper init function, included load stuff into init

--- src/lua/lualib/voix.lua
+++ src/lua/lualib/voix.lua
@@ -1,37 +1,68 @@
 -- basic voix lua config library, they will be much more options later
 require("io")
 
-local voixtools = {}
+local voixtools = {
+	ls = function(path)
+		local t = {}
+		local result = io.popen('ls "' .. path .. '"')
 
-function voixtools.ls(path)
-	local t = {}
-	local result = io.popen('ls "' .. path .. '"')
+		if result == nil then
+			return nil
+		end
 
-	if result == nil then
-		return nil
-	end
+		for i in result:lines() do
+			t[i] = i
+		end
 
-	for i in result:lines() do
-		t[i] = i
-	end
+		result:close()
+		return t
+	end,
 
-	return t
-end
+	hostname = function()
+		local result = io.popen("hostname")
 
-function voixtools.hostname()
-	local result = io.popen("hostname")
-	return result
-end
+		if result == nil then
+			return nil
+		end
+
+		local n = ""
+		for i in result:lines() do
+			n = i
+			break
+		end
+
+		result:close()
+		return n
+	end,
+
+	readlink = function(path)
+		local result = io.popen('readlink -f "' .. path .. '"')
+
+		if result == nil then
+			return nil
+		end
+
+		local n = ""
+		for i in result:lines() do
+			n = i
+			break
+		end
+
+		result:close()
+		return n
+	end,
+}
 
 local voix = {}
 
-function voix:load()
-	self.runit.sv = voixtools.ls("/etc/sv/")
-	self.system.hostname = voixtools.hostname()
-end
-
 function voix:init()
-	return self
+	local obj = self
+
+	obj.runit.sv = voixtools.ls("/etc/sv")
+	obj.system.hostname = voixtools.hostname()
+	obj.system.timezone = voixtools.readlink("/etc/localtime")
+
+	return obj
 end
 
 voix.xbps = {}
