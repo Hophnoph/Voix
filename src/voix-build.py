513791f952d5aa1e483306760fc4aa69103acf94
hophnoph <collectablles@protonmail.com>
2026-05-06 20:22:10 +0100

    made voix-build apply hostname config

--- src/voix-build.py
+++ src/voix-build.py
@@ -2,13 +2,14 @@ from voixlib.lua import read_config
 from rich import print as rprint
 from voixlib.env import update_env, env
 from voixlib.cli import Cli
+from voixlib.apply import apply_hostname
 
 update_env()
 
 
 def now() -> int:
     config = read_config(env["VOIX_CONFIG_PATH"])
-    print(config)
+    apply_hostname(config)
     return 0
 
 
@@ -18,6 +19,6 @@ cli.cmds = {"now": now}
 exit_code = cli.run()
 
 if exit_code == cli.ARGS_EMPTY:
-    pass
+    rprint("[red]you haven't spefied a command[/]")
 elif exit_code == cli.CMD_NOT_FOUND:
     rprint("[red]Command not Found[/]")
