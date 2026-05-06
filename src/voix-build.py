09f56dac84c09c8af2f238fb30eb18362369c13d
hophnoph <collectablles@protonmail.com>
2026-05-06 01:29:40 +0100

    added some basic prototype

--- src/voix-build.py
+++ src/voix-build.py
@@ -1,3 +1,4 @@
+from voixlib.lua import read_config
 from rich import print as rprint
 from voixlib.env import update_env, env
 from voixlib.cli import Cli
@@ -5,7 +6,18 @@ from voixlib.cli import Cli
 update_env()
 
 
+def now() -> int:
+    config = read_config(env["VOIX_CONFIG_PATH"])
+    print(config)
+    return 0
+
+
 cli = Cli()
-cli.cmds = {}
-if not cli.run():
+cli.cmds = {"now": now}
+
+exit_code = cli.run()
+
+if exit_code == cli.ARGS_EMPTY:
+    pass
+elif exit_code == cli.CMD_NOT_FOUND:
     rprint("[red]Command not Found[/]")
